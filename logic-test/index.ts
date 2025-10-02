c6c6bbc59cf2a893de8fa2e27f562140ec61795e
DUPONT Ulysse <UDUPONT-EXT@nexity.fr>
2025-10-02 10:12:22 +0200

    exercice added

---
+++ logic-test/index.ts
@@ -0,0 +1,39 @@
+/* --- 1. Bases --- */
+
+const fruits = {
+  apple: 322,
+  oranges: 56,
+  bananas: 29,
+};
+
+console.log("Fruits:", fruits);
+
+// 1.1 - Retourner un tableau des fruits par ordre de quantité croissante
+// 1.2 - Faire la somme de tous les fruits
+
+const vegetables = {
+  carrots: 77,
+  tomatoes: 209,
+  broccoli: 43.5,
+};
+
+console.log("Légumes:", vegetables);
+
+// 1.3 Filtrer l'objet pour ne garder que les légumes avec une quantité valide (nombre entier)
+// 1.4 Concaténer les fruits et légumes dans un tableau ordonné par ordre alphabétique de la clé
+
+
+
+/* --- 2. TypeScript --- */
+
+// 2.1 Créer une interface Product pour représenter un produit avec un libellé et une quantité et typer les objets avec cette interface
+// 2.2 Créer une fonction typée pour calculer la quantité moyenne par catégorie
+
+const meats = {
+  beef: 0,
+  chicken: 46,
+  fish: null,
+}
+
+console.log("Viandes:", meats);
+
---
+++ my-todo-app/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+// Import the TodoList component (à créer par l’étudiant)
+import TodoList from './components/TodoList.vue'
+
+
+</script>
+
+<template>
+  <!--
+    1. Dans le même projet, créez un composant TodoList minimal :
+        - Affichez une liste de tâches (todo list) gérée avec ref() ou reactive()
+        - Ajoutez un champ pour saisir une nouvelle tâche
+
+    2. Ajoutez un en-tête (header) dans TodoList :
+        - Affichez le nombre d’items avec computed()
+        - Ajoutez un bouton pour trier les tâches par longueur de phrase
+
+    3. Pour chaque todo, affichez un chat aléatoire (image) au chargement du composant :
+        - Utilisez l’API https://cataas.com/cat?json=true pour obtenir une image de chat
+
+    => Modifiez ou remplacez le composant TodoList en dessous pour répondre à ces objectifs.
+  -->
+
+  <TodoList />
+
+</template>
